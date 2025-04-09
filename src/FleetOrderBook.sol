58754da114245d5ceddb9ee497c0dd57155338b2
Tickether <tickether@gmail.com>
2025-04-09 11:15:54 +0000

    chore: contract metadata

--- src/FleetOrderBook.sol
+++ src/FleetOrderBook.sol
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+
 import { ERC6909 } from "solmate/tokens/ERC6909.sol";
+
+
+/// @title 3wb.club fleet order book V1.0
+/// @notice Manages orders for fractional and full investments in 3-wheelers
+/// @author Geeloko
+
 
 contract FleetOrderBook is ERC6909 {
     
